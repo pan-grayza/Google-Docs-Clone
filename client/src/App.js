ae2c2b188d407ab9f6604745e57c466e56b79f46
Pan-Grayza <zagraystepan@gmail.com>
2022-04-29 18:10:55 +0100

    Second Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+import TextEditor from './TextEditor'
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid'
+
 function App() {
-    return <div className="App"></div>
+    return <TextEditor />
 }
 
 export default App
